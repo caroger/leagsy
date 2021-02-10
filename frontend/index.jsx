c6c0d4a9bb602f163c5b2c3f40782c4f31fffbad
caroger <rogerhu1989@gmail.com>
2021-02-10 10:46:03 -0800

    Set up the App and the Root components

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as ApiUtil from "./util/session_api_util";
 import configureStore from "./store/store.js";
-
-const store = configureStore();
+import Root from "./components/root";
 
 document.addEventListener("DOMContentLoaded", () => {
-  // TODO for testing
-  window.login = ApiUtil.login;
-  window.signup = ApiUtil.signup;
-  window.logout = ApiUtil.logout;
+  const store = configureStore();
+  const root = document.getElementById("root");
+
+  // TODO Testing Start
   window.getState = store.getState;
   window.dispatch = store.dispatch;
-  const root = document.getElementById("root");
-  ReactDOM.render(<h1>Welcome to Rtsy!!!</h1>, root);
+
+  //Testing End
+
+  ReactDOM.render(<Root store={store} />, root);
 });
