3b78e2c1fd92fd7d44114a27fad13c471063194e
caroger <rogerhu1989@gmail.com>
2021-02-12 19:32:52 -0800

    fix demo user seeding

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_209_002_125) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+ActiveRecord::Schema.define(version: 2021_02_13_015128) do
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'firstname', null: false
-    t.string 'email', null: false
-    t.string 'password_digest', null: false
-    t.string 'session_token', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['password_digest'], name: 'index_users_on_password_digest', unique: true
-    t.index ['session_token'], name: 'index_users_on_session_token', unique: true
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "firstname", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.string "session_token", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["password_digest"], name: "index_users_on_password_digest", unique: true
+    t.index ["session_token"], name: "index_users_on_session_token", unique: true
   end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+User.create(email: "demo@rtsy.com", firstname:"Demo User", password: "demodemo")
