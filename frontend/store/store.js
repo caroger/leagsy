007289229632dec40667b0b89de0b767ba76c3c8
caroger <rogerhu1989@gmail.com>
2021-02-19 11:57:30 -0800

    configure store for production push

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -4,16 +4,22 @@ import logger from "redux-logger";
 
 import rootReducer from "../reducers/root_reducer";
 
-const middlewares = [thunk, logger];
+const middlewares = [thunk];
+if (process.env.NODE_ENV !== "production") {
+  // must use 'require' (import only allowed at top of file)
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
+}
 
 // TODO get rid of the redux dev tool config for production
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const configureStore = (preloadedState = {}) =>
   createStore(
     rootReducer,
     preloadedState,
-    composeEnhancers(applyMiddleware(...middlewares))
+    // composeEnhancers(applyMiddleware(...middlewares))
+    applyMiddleware(...middlewares)
   );
 
 export default configureStore;
