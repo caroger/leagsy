c83f7ad36489c5cdab514c266d5e76e425856c16
Roger Hu <rogerhu1989@gmail.com>
2021-04-24 18:38:16 -0700

    update devtool config optimized for prod env

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,33 @@
 const path = require("path");
 
-module.exports = {
-  context: __dirname,
-  entry: "/frontend/index.jsx",
-  output: {
-    path: path.resolve(__dirname, "app", "assets", "javascripts"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: "babel-loader",
-          query: {
-            presets: ["@babel/env", "@babel/react"],
+module.exports = (env, argv) => {
+  const mode = argv.mode || "development";
+
+  const config = {
+    context: __dirname,
+    entry: "/frontend/index.jsx",
+    output: {
+      path: path.resolve(__dirname, "app", "assets", "javascripts"),
+      filename: "bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: "babel-loader",
+            query: {
+              presets: ["@babel/env", "@babel/react"],
+            },
           },
         },
-      },
-    ],
-  },
-  devtool: "source-map",
-  resolve: {
-    extensions: [".js", ".jsx", "*"],
-  },
+      ],
+    },
+    devtool: mode === "development" ? "cheap-module-eval-source-map" : false,
+    resolve: {
+      extensions: [".js", ".jsx", "*"],
+    },
+  };
+  return config;
 };
